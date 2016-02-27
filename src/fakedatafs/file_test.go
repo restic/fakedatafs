f429c2a1d62d21af661ef875b5b0dc6bf3a77987
Alexander Neumann <alexander@bumpern.de>
2016-02-27 17:42:11 +0100

    fix test for file

--- src/fakedatafs/file_test.go
+++ src/fakedatafs/file_test.go
@@ -1,25 +1,20 @@
 package main
 
-import (
-	"io"
-	"io/ioutil"
-	"testing"
-)
+import "testing"
 
 var testFileSizes = []int{0, 100, 200, 500, 1024, 7666, 1 << 20}
 
 func TestFile(t *testing.T) {
 	for i, filesize := range testFileSizes {
 		f := NewFile(23, filesize, 23)
-
-		n, err := io.Copy(ioutil.Discard, f)
+		buf, err := f.ReadAll()
 		if err != nil {
 			t.Errorf("test %d: error %v", i, err)
 			continue
 		}
 
-		if n != int64(filesize) {
-			t.Errorf("test %d: invalid number of bytes returned, want %d, got %d", i, filesize, n)
+		if len(buf) != filesize {
+			t.Errorf("test %d: invalid number of bytes returned, want %d, got %d", i, filesize, len(buf))
 			continue
 		}
 	}
