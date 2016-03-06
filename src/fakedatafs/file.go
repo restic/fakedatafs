fdfcbbed93b39783f9a9729171f367e6bb8cc7cd
Alexander Neumann <alexander@bumpern.de>
2016-03-06 13:59:56 +0100

    Add segments

--- src/fakedatafs/file.go
+++ src/fakedatafs/file.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"math/rand"
 
 	"bazil.org/fuse"
@@ -9,28 +12,183 @@ import (
 	"golang.org/x/net/context"
 )
 
+const minSegmentSize = 512 * 1024      // 512KiB
+const maxSegmentSize = 4 * 1024 * 1024 // 4MiB
+
+// Segment is one segment within a file.
+type Segment struct {
+	Seed int64
+	Size int
+}
+
+func (s Segment) String() string {
+	return fmt.Sprintf("<Segment seed 0x%x, len %v>", s.Seed, s.Size)
+}
+
+type randReader struct {
+	rd  io.Reader
+	buf []byte
+}
+
+func newRandReader(rd io.Reader) io.Reader {
+	return &randReader{rd: rd, buf: make([]byte, 0, 7)}
+}
+
+func (rd *randReader) Read(p []byte) (int, error) {
+	// first, copy buffer to p
+	pos := copy(p, rd.buf)
+
+	// if this is enough to fill p, return
+	if pos == len(p) {
+		return pos, nil
+	}
+
+	// else p is larger than buf, set length to zero
+	rd.buf = rd.buf[:0]
+
+	// load multiple of 7 byte to temp buffer
+	bufsize := ((len(p) / 7) + 1) * 7
+	buf := make([]byte, bufsize)
+	n, err := io.ReadFull(rd.rd, buf)
+	if err != nil {
+		return pos, err
+	}
+
+	// copy the buffer to p
+	n = copy(p[pos:], buf)
+	pos += n
+
+	// save the remaining bytes in rd.buf
+	rd.buf = rd.buf[:7]
+	n = copy(rd.buf, buf[len(p):])
+	rd.buf = rd.buf[:n]
+
+	return pos, nil
+}
+
+// Reader returns a reader for this segment.
+func (s Segment) Reader() io.Reader {
+	return newRandReader(rand.New(rand.NewSource(s.Seed)))
+}
+
 // File represents fake data with a specific seed.
 type File struct {
-	Seed int64
-	Size  int
-	Inode uint64
+	Seed     int64
+	Size     int
+	Inode    uint64
+	Segments []Segment
+}
+
+func (f File) String() string {
+	return fmt.Sprintf("<File seed 0x%x, Size %d>", f.Seed, f.Size)
 }
 
 // NewFile initializes a new file with the given seed.
 func NewFile(seed int64, size int, inode uint64) *File {
-	return &File{
+	f := &File{
 		Seed:  seed,
-		Size:  size,
 		Inode: inode,
 	}
+
+	fmt.Printf("\nnew file, seed 0x%x, size %v\n", seed, size)
+
+	src := rand.New(rand.NewSource(seed))
+	var segmentIndex int64
+	for f.Size < size {
+		nextSize := size - f.Size
+		if nextSize > minSegmentSize {
+			max := nextSize - minSegmentSize
+			if max > maxSegmentSize {
+				max = maxSegmentSize
+			}
+			nextSize = src.Intn(max) + minSegmentSize
+		}
+
+		segment := Segment{
+			Seed: seed ^ segmentIndex,
+			Size: nextSize,
+		}
+		f.Segments = append(f.Segments, segment)
+
+		fmt.Printf("  generate segment %v, %v\n", segmentIndex, segment)
+
+		f.Size += nextSize
+		segmentIndex++
+	}
+
+	return f
 }
 
 // ReadAll returns the content of the file.
 func (f File) ReadAll() ([]byte, error) {
 	buf := make([]byte, f.Size)
-	src := rand.New(rand.NewSource(f.Seed))
-	_, err := io.ReadFull(src, buf)
-	return buf, err
+	pos := 0
+	for _, seg := range f.Segments {
+		n, err := io.ReadFull(seg.Reader(), buf[pos:pos+seg.Size])
+		pos += n
+		if err != nil {
+			return buf[:pos], err
+		}
+	}
+
+	if pos != f.Size {
+		return buf, io.ErrUnexpectedEOF
+	}
+
+	return buf, nil
+}
+
+// ReadAt reads the content at the offset.
+func (f File) ReadAt(p []byte, off int64) (n int, err error) {
+	fmt.Printf("ReadAt(len %v, off %v)\n", len(p), off)
+	if off < 0 {
+		return 0, errors.New("not implemented")
+	}
+
+	if off > int64(f.Size) {
+		return 0, errors.New("offset beyond end of file")
+	}
+
+	pos := 0
+	for i, seg := range f.Segments {
+		if off > int64(seg.Size) {
+			fmt.Printf("  skip segment %v, off %v, size %v\n", i, off, seg.Size)
+			off -= int64(seg.Size)
+			continue
+		}
+
+		fmt.Printf("   found segment %v, off %v, size %v\n", i, off, seg.Size)
+
+		maxRead := pos + seg.Size - int(off)
+		if maxRead > len(p) {
+			maxRead = len(p)
+		}
+
+		rd := seg.Reader()
+		if off > 0 {
+			fmt.Printf("    discard %d bytes\n", off)
+			_, err = io.CopyN(ioutil.Discard, rd, off)
+			if err != nil {
+				return 0, err
+			}
+
+			off = 0
+		}
+
+		fmt.Printf("     read to p[%d:%d], len(p) %v\n", pos, maxRead, len(p))
+		n, err := io.ReadFull(rd, p[pos:maxRead])
+		fmt.Printf("      -> n %v, err %v\n", n, err)
+		pos += n
+		if err != nil {
+			return pos, err
+		}
+
+		if pos == len(p) {
+			break
+		}
+	}
+
+	return pos, nil
 }
 
 // Attr returns the attributes for this file.
--- src/fakedatafs/file_test.go
+++ src/fakedatafs/file_test.go
@@ -1,10 +1,18 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"math/rand"
+	"testing"
+)
 
-var testFileSizes = []int{0, 100, 200, 500, 1024, 7666, 1 << 20}
+// var testFileSizes = []int{0, 100, 200, 500, 1024, 7666, 1 << 20, 1 << 24}
+var testFileSizes = []int{1 << 20}
 
 func TestFile(t *testing.T) {
+	rnd := rand.New(rand.NewSource(23))
+
 	for i, filesize := range testFileSizes {
 		f := NewFile(23, filesize, 23)
 		buf, err := f.ReadAll()
@@ -16,6 +24,104 @@ func TestFile(t *testing.T) {
 		if len(buf) != filesize {
 			t.Errorf("test %d: invalid number of bytes returned, want %d, got %d", i, filesize, len(buf))
 			continue
+		}
+
+		buf2 := make([]byte, filesize)
+		n, err := f.ReadAt(buf2, 0)
+
+		if err != nil {
+			t.Errorf("test %d: error %v", i, err)
+			continue
+		}
+
+		if n != filesize {
+			t.Errorf("test %d: invalid number of bytes returned, want %d, got %d", i, filesize, n)
+			continue
+		}
+
+		if !bytes.Equal(buf, buf2) {
+			t.Errorf("test %d: wrong bytes returned", i)
+			fmt.Printf("test %d: wrong bytes returned\n", i)
+			continue
+		}
+
+		if filesize == 0 {
+			n, err = f.ReadAt(buf2, 23)
+			if n != 0 {
+				t.Errorf("test %d: to read 0 bytes from empty file, got %d", i, n)
+			}
+
+			if err == nil {
+				t.Errorf("test %d: expected error for empty file at offset 23 not found", i)
+			}
+
+			continue
+		}
+
+		for j := 0; j < 10; j++ {
+			fmt.Printf("\n=========================================\n")
+
+			o := rnd.Intn(filesize)
+			l := rnd.Intn(filesize - o)
+
+			fmt.Printf("  filesize %v, len %v, off %v\n", filesize, l, o)
+
+			readatBuf := make([]byte, l)
+			n, err = f.ReadAt(readatBuf, int64(o))
+			fmt.Printf("n %v, l %v, o %v\n", n, l, o)
+			if err != nil {
+				t.Errorf("test %d/%d: reading len %v bytes at offset %v failed: %v", i, j, l, o, err)
+				continue
+			}
+
+			if n != l {
+				t.Errorf("test %d/%d: want %d bytes, got %d", i, j, l, n)
+				fmt.Printf("test %d/%d: want %d bytes, got %d\n", i, j, l, n)
+				continue
+			}
+
+			if !bytes.Equal(readatBuf, buf[o:o+l]) {
+				fmt.Printf("------------------ test %d/%d: wrong bytes returned at offset %v, len %v\n", i, j, o, l)
+				// fmt.Printf("  want: %02x\n", buf[o:o+l])
+				// fmt.Printf("   got: %02x\n", readatBuf)
+				t.Errorf("test %d/%d: wrong bytes returned at offset %v, len %v", i, j, o, l)
+			}
+		}
+	}
+}
+
+func TestRandReader(t *testing.T) {
+	rd := newRandReader(rand.New(rand.NewSource(23)))
+
+	buf1 := make([]byte, 200)
+	n, err := rd.Read(buf1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if n != len(buf1) {
+		t.Fatalf("not enough bytes returned, want %d, got %d", len(buf1), n)
+	}
+
+	for _, l := range []int{1, 2, 3, 4, 5, 6, 7, 20, 22} {
+		buf2 := make([]byte, l)
+		rd := newRandReader(rand.New(rand.NewSource(23)))
+
+		for i := 0; i < 2; i++ {
+			n, err := rd.Read(buf2)
+			if err != nil {
+				t.Error(err)
+				continue
+			}
+
+			if n != len(buf2) {
+				t.Errorf("not enough bytes returned, want %d, got %d", len(buf1), n)
+				continue
+			}
+
+			if !bytes.Equal(buf1[i*l:(i+1)*l], buf2) {
+				t.Errorf("bytes not equal: want %02x, got %02x", buf1[i*l:(i+1)*l], buf2)
+			}
 		}
 	}
 }
